22bfbdaa5a97e09c39ff36366ecb0a5bd4b12ddb
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2020-04-09 16:36:10 -0400

    add fetch to post request

--- controllers/generateController.js
+++ controllers/generateController.js
@@ -1,9 +1,24 @@
-var { getQRCode } = require("../helpers");
+const { generateFetchUrl, getQRCode } = require("../helpers");
+const fs = require("fs");
 
 const create = async (request, response) => {
   const { url } = request.body;
-  const qrCode = await getQRCode(url);
-  return response.json({ qrCode });
+  const { accept } = request.headers;
+  if (accept === "text/html") {
+    response.set("Content-Type", accept);
+    return response.send(`<img src="${generateFetchUrl(url)}"/>`);
+  } else {
+    await getQRCode(url);
+    fs.readFile("./tmpCodeImage.png", (err, data) => {
+      response.set("Content-Type", "image/png");
+      return response.send(data);
+    });
+  }
+  // URL validation (regex) (http|https)
+  // invalid input: 422 (unprocessable entity)
+
+  // adding a check here to see if we have the image first
+  // before making the request
 };
 
 module.exports = { create };
--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,15 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+
 const generateFetchUrl = data => {
   return `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`;
 };
 
 const getQRCode = async url => {
-  return fetch(generateFetchUrl(url));
+  return fetch(generateFetchUrl(url)).then(response => {
+    const dest = fs.createWriteStream("./tmpCodeImage.png");
+    response.body.pipe(dest);
+  });
 };
 
-module.exports = { getQRCode };
+module.exports = { generateFetchUrl, getQRCode };
